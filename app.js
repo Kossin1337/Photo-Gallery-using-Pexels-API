b827fd1770b7458d8a163c33d0b6726f998f0d40
Kossin1337 <piotrswobodzinski@gmail.com>
2021-04-07 14:53:00 +0200

    main functionality implemented

--- app.js
+++ app.js
@@ -1,34 +1,108 @@
 const API_KEY = "563492ad6f91700001000001d448a99e8e6342cb8777d11872134b98";
 const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
-const submitButton = document.querySelector(".submit-button");
-let searchValue;
+const form = document.querySelector(".search-form");
+const scrollBtn = document.getElementById("scroll-to-top");
+let searchValue; // currently empty - will contain search value result
 
-async function curatedPhotos() {
-  const dataFetch = await fetch(
-    "https://api.pexels.com/v1/curated?per_page=20",
-    {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        Authorization: API_KEY,
-      },
-    }
-  );
+const moreButton = document.querySelector(".more-button");
+let pagePhotos = 20; // stores the ammount of page photos to get
+let page = 1;
+let fetchLink;
+let currentSearch;
+
+/* input event Listener */
+searchInput.addEventListener("input", updateInput);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  currentSearch = searchValue;
+  searchPhotos(searchValue);
+});
+
+/* scroll to top */
+window.addEventListener("scroll", function () {
+  scrollBtn.classList.toggle("active", window.scrollY > 500);
+});
+/* scroll back to top on click */
+scrollBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+/* load more content button */
+moreButton.addEventListener("click", () => {
+  loadMore();
+});
+
+function updateInput(e) {
+  searchValue = e.target.value;
+}
+
+async function fetchApi(url) {
+  const dataFetch = await fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: API_KEY,
+    },
+  });
   const data = await dataFetch.json();
+  return data;
+}
+
+function generatePictures(data) {
   data.photos.forEach((photo) => {
-    console.log(photo);
+    // console.log(photo);
     const galleryImage = document.createElement("div");
     galleryImage.classList.add("gallery-img");
     galleryImage.innerHTML = `
-    <img src="${photo.src.medium}"> </img>
-    <h4><a href="${photo.photographer_url}">${photo.photographer} </a></h4>`;
+    <div class="gallery-image" style="background-image: url(${photo.src.large});">
+      <div class="image-info active">
+        <h4 class="photographer"><a href="${photo.photographer_url}">${photo.photographer} </a></h4>
+        <div class="info-wrapper">
+          <a href="${photo.src.original}" class="download-img"><i class="fas fa-download"></i></a>
+          <span class="like"><i class="far fa-heart"></i></span>
+          <span class="img-resolution">${photo.width}x${photo.height}</span>
+        </div> 
+      </div>
+    </div>
+    `;
     gallery.appendChild(galleryImage);
   });
 }
 
-// async function searchPhotos() {
+async function curatedPhotos() {
+  fetchLink = `https://api.pexels.com/v1/curated?per_page=${pagePhotos}`;
+  const data = await fetchApi(fetchLink);
+  generatePictures(data);
+}
 
-// }
+async function searchPhotos(query) {
+  clearImages();
+  fetchLink = `https://api.pexels.com/v1/search?query=${query}&per_page=${pagePhotos}`;
+  const data = await fetchApi(fetchLink);
+  generatePictures(data);
+}
+
+function clearImages() {
+  gallery.innerHTML = "";
+  searchInput.value = "";
+}
+
+async function loadMore() {
+  page++;
+  if (currentSearch) {
+    fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=${
+      pagePhotos * page
+    }`;
+  } else {
+    fetchLink = `https://api.pexels.com/v1/curated?per_page=${
+      pagePhotos * page
+    }`;
+  }
+  const data = await fetchApi(fetchLink);
+  clearImages();
+  generatePictures(data);
+}
 
 curatedPhotos();
---
+++ readme.md
@@ -0,0 +1,6 @@
+My first project working with API's & fetching data and using async await
+I plan on adding:
+search functionality responsive navbar menu with most popular searches
+searching photos by color
+like system and adding photo to your currently liked ones
+Animations and more css styling
